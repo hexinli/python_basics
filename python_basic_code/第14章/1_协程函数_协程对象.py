f60237fd83c8961ff2ef7cf45bd5b2b88920633a
hexinli <hexinli2005@163.com>
2026-07-20 16:36:00 +0800

    协程

--- python_basic_code/第14章/1_协程函数_协程对象.py
+++ python_basic_code/第14章/1_协程函数_协程对象.py
@@ -20,6 +20,7 @@ print(coroutine_object)
 # 1.创建一个事件循环。
 # 2.将接收到的协程对象，包装成一个任务(task),交给事件循环。
 # 3.启动事件循环。
+# 注意：asyncio.run会阻塞当前线程，直到任务执行完毕，并返回该任务return的最终结果
 result = asyncio.run(coroutine_object)
 
 print(result)
