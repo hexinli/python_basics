fffdc2bd71a2d334fb23136f4fe84c81343c3f03
hexinli <hexinli2005@163.com>
2026-07-21 15:56:18 +0800

    协程

--- python_basic_code/第14章/7_使用协程下载图片.py
+++ python_basic_code/第14章/7_使用协程下载图片.py
@@ -1,18 +1,23 @@
+import asyncio
 import time
 
-import requests
+import aiohttp
 
 
-def download_picture(url, index):
+async def download_picture(session, index, url):
     print(f"开始下载：{url}")
-    # 发送网络请求，获取这张图片
-    response = requests.get(url)
+    # 发送网络请求，获取这张图片，请求发送出去后，要等待服务器把数据返回，等的这段事件就是IO等待
+    response = await session.get(url)
+    # 等待数据(图片数据可能分多次传输，需要等待数据全部读完，等的这段时间也是IO等待)
+    content = await response.read()
     print(f"下载完毕")
     with open(f"{index}.jpg", "wb") as file:
-        file.write(response.content)
+        file.write(content)
+    # 释放连接资源(告诉aiohttp，这个连接我不用了，你可以回收了)
+    await  response.release()
 
 
-def main():
+async def main():
     stat = time.time()
     url_list = [
         "https://picsum.photos/800/600?random=1",
@@ -36,10 +41,16 @@ def main():
         "https://picsum.photos/800/600?random=19",
         "https://picsum.photos/800/600?random=20"
     ]
-    for index, url in enumerate(url_list):
-        download_picture(url, index)
+    # 创建会话对象(发送请求的工具)
+    session = aiohttp.ClientSession()
+    # 创建多个协程对象
+    coroutine_list = [download_picture(session, index, url) for index, url in enumerate(url_list)]
+    # 将多个协程对象交给事件循环
+    await asyncio.gather(*coroutine_list)
+    # 关闭会话
+    await session.close()
     print(f"总花费时间{time.time() - stat}")
 
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
